5f20bc64fdcd1fae3b3fa0e602e69afb84c4e968
EQwon <bluetwintail0906@gmail.com>
2020-12-19 22:44:35 +0900

    Create MySortedList.cs

---
+++ DataStructure/MySortedList/MySortedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure.MySortedList
+{
+    public class MySortedList<TKey, TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+        private int _size;
+        private IComparer<TKey> comparer;
+
+        static TKey[] emptyKeys = new TKey[0];
+        static TValue[] emptyValues = new TValue[0];
+
+        #region 생성자 (Constructor)
+        public MySortedList()
+        {
+            keys = emptyKeys;
+            values = emptyValues;
+            _size = 0;
+            comparer = Comparer<TKey>.Default;
+        }
+        #endregion
+    }
+}
